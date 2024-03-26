2c6017bc0599c8ae698752268a1cd1fc60af32f3
Adam Hughes <9903835+tri-adam@users.noreply.github.com>
2024-03-26 15:44:07 +0000

    lint: add new linters
    Enable copyloopvar and intrange linters. They won't activate until the
    module adopts Go 1.22, but run cleanly at the moment.

--- pkg/mutate/squashfs_test.go
+++ pkg/mutate/squashfs_test.go
@@ -160,8 +160,6 @@ func Test_SquashfsLayer(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		tt := tt
-
 		t.Run(tt.name, func(t *testing.T) {
 			if _, err := exec.LookPath(tt.converter); errors.Is(err, exec.ErrNotFound) {
 				t.Skip(err)
